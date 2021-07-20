95fada3c11b8dae5ac1b8c2a16474ee6e34ee98a
LixPy <LixPy@protonmail.ch>
2021-07-20 07:40:03 -0400

    Typo correction. Joke Api removed. Base64 Encode fixed. space ignoreing fixed.

--- DecodeEncode.py
+++ DecodeEncode.py
@@ -7,7 +7,6 @@ import urllib.parse
 def hexDecode(arg):
     return bytes.fromhex(arg).decode("UTF-8")
 
-
 def hexEncode(arg):
     return codecs.encode(arg, "hex_codec").decode("UTF-8")
 
@@ -15,7 +14,6 @@ def hexEncode(arg):
 # Rot13
 def rot13Decode(arg):
     return codecs.decode(arg, "rot_13")
-
 
 def rot13Encode(arg):
     return codecs.encode(arg, "rot_13")
@@ -25,15 +23,13 @@ def rot13Encode(arg):
 def base64Decode(arg):
     return base64.b64decode(arg).decode("UTF-8")
 
-
 def base64Encode(arg):
-    return base64.b64encode(arg)
+    return base64.b64encode(bytes(arg.encode())).decode()
 
 
 # URL
 def urlEncode(arg):
     return urllib.parse.quote(str)
-
 
 def urlDecode(arg):
     return urllib.parse.unquote(str)
